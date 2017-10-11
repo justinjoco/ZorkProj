6c11c41680a843bedfb269d73ccb0663a8049733
JustinJoco <justin.joco@gmail.com>
2017-10-11 11:46:11 -0400

    Added gitignore for XCode

---
+++ ZorkProj/ZorkProj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ZorkProj
+//
+//  Created by Justin-Anthony Ampil Joco on 10/11/17.
+//  Copyright © 2017 Justin-Anthony Ampil Joco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
